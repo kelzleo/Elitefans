f3880ab855ecd215815c0e14a1d0da4956923f89
Kelzleo <95962745+kelzleo@users.noreply.github.com>
2025-04-27 16:13:56 +0600

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// config.js (in the parent directory)
-module.exports = {
-    flutterwaveSecretKey: 'FLWSECK-370c5a77f39d70e9868223c001fc2812-1948ccccec5vt-X',
-    flutterwavePublicKey: 'FLWPUBK-c0a9315e34d2b02369494d7c5640e654-X'
-  };
-  
